0249abc2d41d9fffc61144f3ce3aac23b04663e2
MeetMahadevwala <meetmahadevwala@gmail.com>
2025-08-03 18:59:43 +0530

    configure dynamic API URL for frontend

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,23 +1,23 @@
 import axios from "axios";
-// import { config } from "dotenv";
+import { config } from "dotenv";
+const token = localStorage.getItem('jwt');
 
 const apiService = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: process.env.REACT_APP_API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+        ...(token && { Authorization: `Bearer ${token}`}),
+    },
 });
 
-apiService.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-
-        if(token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+apiService.interceptors.request.use((config) => {
+    const token = localStorage.getItem('jwt');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
-);
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 
 export default apiService;
